e6fc174d37169b130a269da2d015f74fd2e79d90
SaloniSS <sxs170015@utdallas.edu>
2020-07-04 18:24:13 -0500

    Linked firebase

--- app/Chat.js
+++ app/Chat.js
@@ -1,32 +1,67 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
+import { Dialogflow_V2 } from "react-native-dialogflow";
+import { dialogflowConfig } from "./env";
 
 export function Chat() {
   const [messages, setMessages] = useState([]);
 
+  const BOT_USER = {
+    _id: 2,
+    name: "Helper",
+    avatar:
+      "https://cdn.discordapp.com/attachments/622809737435676677/711452177524195328/hdt_logo.png",
+  };
+
   useEffect(() => {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+
     setMessages([
       {
         _id: 1,
         text:
           "Hello friend! My name is <name> and I'm here to help you! How was your day today?",
         createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "Helper",
-          avatar:
-            "https://cdn.discordapp.com/attachments/622809737435676677/711452177524195328/hdt_logo.png",
-        },
+        user: BOT_USER,
       },
     ]);
   }, []);
 
-  const onSend = useCallback((messages = []) => {
-    console.log(messages);
+  const onSend = (messages = []) => {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages)
     );
-  }, []);
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      (result) => handleGoogleResponse(result),
+      (error) => console.log(error)
+    );
+  };
+
+  const handleGoogleResponse = (result) => {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    sendBotResponse(text);
+  };
+
+  const sendBotResponse = (text) => {
+    let msg = {
+      _id: messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER,
+    };
+
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, [msg])
+    );
+  };
 
   return (
     <GiftedChat
