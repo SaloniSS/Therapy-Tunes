1b88f6cb656618be9aae87f54b1613810b9092c4
SaloniSS <sxs170015@utdallas.edu>
2020-07-04 23:47:10 -0500

    Backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
-var SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
+var cors = require("cors");
+const SpotifyWebApi = require("spotify-web-api-node");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
@@ -7,19 +9,39 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.clientSecret,
 });
 
-// Retrieve an access token
-spotifyApi.clientCredentialsGrant().then(
-  function (data) {
-    console.log("The access token expires in " + data.body["expires_in"]);
-    console.log("The access token is " + data.body["access_token"]);
+const app = express();
 
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body["access_token"]);
-  },
-  function (err) {
-    console.log(
-      "Something went wrong when retrieving an access token",
-      err.message
-    );
-  }
-);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/spotify-token", (req, res) => {
+  // Retrieve an access token
+  spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+      console.log("The access token expires in " + data.body["expires_in"]);
+      console.log("The access token is " + data.body["access_token"]);
+
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body["access_token"]);
+
+      res.status(200).send(data.body["access_token"]).end();
+    },
+    function (err) {
+      console.log(
+        "Something went wrong when retrieving an access token",
+        err.message
+      );
+      res.status(500).send("Error").end();
+    }
+  );
+});
+
+app.use(express.json());
+
+const PORT = 8000;
+
+app.listen(PORT, console.log(`Server running mode on port ${PORT}`));
